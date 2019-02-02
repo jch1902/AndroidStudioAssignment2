088c8efe2b1b1ae1f44344abf716c1d12237757e
jch1902 <jasper.d39c@gmail.com>
2019-02-02 13:30:13 -0800

    second commit

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -1,16 +1,75 @@
+/*
+CSE41246 - Android Java Fundamentals
+Winter 2019
+Assignment #2
+Jasper Hoong
+ */
 package com.example.assignment2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int min = 1;
+    private int max = 10;
 
     //Widgets
-    
+    private EditText minEditText = null;
+    private EditText maxEditText = null;
+    private TextView results = null;
+    private Button generate = null;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Set the default numbers
+        setDefaultMinMax();
+       //Initialize
+        this.minEditText = (EditText) findViewById(R.id.editMinimum);
+        this.maxEditText = (EditText) findViewById(R.id.editMaximum);
+        this.results = findViewById(R.id.result);
+        this.generate = (Button) findViewById(R.id.button);
+        //Generate
+        this.generate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchValuesFromFields();
+                generateRandomNumber();
+
+            }
+        });
+
+
+
+    }
+    private void setDefaultMinMax(){
+        //Convert the values to strings
+        this.minEditText.setText(Integer.toString(min));
+        this.maxEditText.setText(Integer.toString(max));
+    }
+    private void fetchValuesFromFields(){
+        //Get the numbers from the text boxes
+        this.min =  Integer.parseInt(this.minEditText.getText().toString());
+        this.max = Integer.parseInt(this.maxEditText.getText().toString());
+    }
+    private void generateRandomNumber(){
+        //Get the random number
+        int randomNumber = getRandomInt(min,max);
+        String randomString = Integer.toString(randomNumber);
+        results.setText(randomString);
+    }
+    private int getRandomInt(int mini, int maxi){
+       //Generate the random number
+        Random randomNumber = new Random();
+        return randomNumber.nextInt(maxi) + mini;
     }
 }
